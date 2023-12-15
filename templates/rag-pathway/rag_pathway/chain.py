4c9e23a13deaaf7d3aba08b96f62fbd3c7b915f3
mlewandowski <mlewandowski@MacBook-Pro-mlewandowski.local>
2023-12-15 23:29:43 +0100

    Pathway vectorstore and rag-pathway template

---
+++ templates/rag-pathway/rag_pathway/chain.py
@@ -0,0 +1,87 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.pydantic_v1 import BaseModel
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableParallel, RunnablePassthrough
+
+from langchain.vectorstores.pathway import (
+    PathwayVectorClient,
+    PathwayVectorServer,
+)
+
+# If you have a running Pathway Vectorstore instance you can connect to it via client. If not, you can run Vectorstore as follows:
+create_vectorstore = True
+if create_vectorstore:
+    # Example for document loading (from local folders), splitting, and creating vectorstore with Pathway
+    import pathway as pw
+    from langchain.text_splitter import CharacterTextSplitter
+    from langchain.embeddings import OpenAIEmbeddings
+
+    HOST = "127.0.0.1"
+    PORT = "8780"
+
+    data_sources = []
+    data_sources.append(
+        pw.io.fs.read(
+            "./sample_documents", format="binary", mode="streaming", with_metadata=True
+        )  # This creates a `pathway` connector that tracks all the files in the sample_documents directory
+    )
+
+    # This creates a connector that tracks files in Google drive.
+    # please follow the instructions at https://pathway.com/developers/tutorials/connectors/gdrive-connector/ to get credentials
+    # See pathway documentation for more options including s3, Dropbox, etc.
+    # data_sources.append(
+    #     pw.io.gdrive.read(object_id="17H4YpBOAKQzEJ93xmC2z170l0bP2npMy", service_user_credentials_file="credentials.json", with_metadata=True))
+
+    # Split
+    from langchain.text_splitter import CharacterTextSplitter
+
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n",
+        chunk_size=10,
+        chunk_overlap=5,
+        length_function=len,
+        is_separator_regex=False,
+    )
+
+    # Embed
+    embeddings_model = OpenAIEmbeddings()
+
+    # Launch VectorDB
+    vector_server = PathwayVectorServer(
+        *data_sources, embedder=embeddings_model, splitter=text_splitter
+    )
+    vector_server.run_server(host=HOST, port=PORT, threaded=True)
+
+# Initalize client
+client = PathwayVectorClient(
+    host=HOST,
+    port=PORT,
+)
+
+retriever = client.as_retriever()
+
+
+# RAG prompt
+template = """Answer the question based only on the following context:
+{context}
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+# RAG
+model = ChatOpenAI()
+chain = (
+    RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+
+# Add typing for input
+class Question(BaseModel):
+    __root__: str
+
+
+chain = chain.with_types(input_type=Question)
