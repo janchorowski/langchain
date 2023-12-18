5b04ea3b895518e237d37bb92c08c8007ef1530b
Berke <berkecanrizai1@gmail.com>
2023-12-18 12:47:53 +0100

    fix: linting

--- templates/rag-pathway/rag_pathway/chain.py
+++ templates/rag-pathway/rag_pathway/chain.py
@@ -3,38 +3,42 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.pydantic_v1 import BaseModel
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableParallel, RunnablePassthrough
-
-from langchain.vectorstores import (
-    PathwayVectorClient,
-    PathwayVectorServer,
-)
+from langchain.vectorstores import PathwayVectorClient, PathwayVectorServer
 
 HOST = "127.0.0.1"
 PORT = "8780"
 
-# If you have a running Pathway Vectorstore instance you can connect to it via client. If not, you can run Vectorstore as follows:
+# If you have a running Pathway Vectorstore instance you can connect to it via client. 
+# If not, you can run Vectorstore as follows:
 create_vectorstore = True
 if create_vectorstore:
-    # Example for document loading (from local folders), splitting, and creating vectorstore with Pathway
-    import pathway as pw
-    from langchain.text_splitter import CharacterTextSplitter
+    # Example for document loading (from local folders), splitting, 
+    # and creating vectorstore with Pathway
     from langchain.embeddings import OpenAIEmbeddings
+    from langchain.text_splitter import CharacterTextSplitter
+
+    import pathway as pw
 
     data_sources = []
     data_sources.append(
         pw.io.fs.read(
             "./sample_documents", format="binary", mode="streaming", with_metadata=True
-        )  # This creates a `pathway` connector that tracks all the files in the sample_documents directory
+        )  # This creates a `pathway` connector that tracks 
+        # all the files in the sample_documents directory
     )
 
     # This creates a connector that tracks files in Google drive.
-    # please follow the instructions at https://pathway.com/developers/tutorials/connectors/gdrive-connector/ to get credentials
-    # See pathway documentation for more options including s3, Dropbox, etc.
-    # data_sources.append(
-    #     pw.io.gdrive.read(object_id="17H4YpBOAKQzEJ93xmC2z170l0bP2npMy", service_user_credentials_file="credentials.json", with_metadata=True))
-
-    # Split
-    from langchain.text_splitter import CharacterTextSplitter
+    # please follow the instructions at:
+    # https://pathway.com/developers/tutorials/connectors/gdrive-connector/ 
+    # to get credentials, see pathway documentation for more options including:
+    # s3, Dropbox, etc.
+    data_sources.append(
+        pw.io.gdrive.read(
+            object_id="17H4YpBOAKQzEJ93xmC2z170l0bP2npMy",
+            service_user_credentials_file="credentials.json",
+            with_metadata=True,
+        )
+    )
 
     text_splitter = CharacterTextSplitter(
         separator="\n\n",
